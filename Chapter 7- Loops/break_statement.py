f1651190634ff57acfd47eec64e0aca1df7c104d
Sugam Sinjali <141037350+Sinjali777@users.noreply.github.com>
2025-10-12 19:44:57 +0545

    update

---
+++ Chapter 7- Loops/break_statement.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    print(i)
+    if i==5:
+        break # break -> exits the loop
+else:
+    print("This is the part for else") # doesn't come because loop isn't in successful execution.
+    
