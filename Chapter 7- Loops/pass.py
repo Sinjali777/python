f20ea4ef118a407b9d80f20f187eed3e52eeacae
Sugam Sinjali <141037350+Sinjali777@users.noreply.github.com>
2025-10-12 19:49:19 +0545

    update

---
+++ Chapter 7- Loops/pass.py
@@ -0,0 +1,12 @@
+#pass is a null statement in python. It instructs to "do nothing".
+i=4
+
+def run(player): #these can be used to make skeleton understanding of what functions to use.
+    pass
+def ouch(player): 
+    pass
+if i>0:
+    pass
+while i>6:
+    pass
+print("Sugam is Hero")
