2328c7c4dbc2ef297f9c1e757e10858dc28449cc
Sugam Sinjali <141037350+Sinjali777@users.noreply.github.com>
2025-10-14 22:24:16 +0545

    update

--- Chapter 7- Loops/ques3.py
+++ Chapter 7- Loops/ques3.py
@@ -1,5 +1,5 @@
-# multiplication using while
-num=int(input("Enter any number"))
+# multiplication using while with any number given by the user
+num=int(input("Enter any number for multiplication"))
 i=1
 while i<=10:
     print(f"{num}x{i}={num*i}")
