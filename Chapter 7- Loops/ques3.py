a80bcecf1f932954de0939d4b8b8694f382d107c
Sugam Sinjali <141037350+Sinjali777@users.noreply.github.com>
2025-10-18 23:00:31 +0545

    update

--- Chapter 7- Loops/ques3.py
+++ Chapter 7- Loops/ques3.py
@@ -2,5 +2,5 @@
 num=int(input("Enter any number for multiplications"))
 i=1
 while i<=10:
-    print(f"{num}x{i}={num*i}") #this gives the table of the number
+    print(f"{num}x{i}={num*i}") #this gives the table of the numbers
     i=i+1 
