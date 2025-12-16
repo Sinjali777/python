ebf7de925f988962456531bb6aa64241df4b64b3
Sugam Sinjali <141037350+Sinjali777@users.noreply.github.com>
2025-12-16 22:37:48 +0545

    update

---
+++ Chapter 7- Loops/ques4.py
@@ -0,0 +1,12 @@
+#prime or not
+num= int(input("enter the number :"))
+prime=True
+for i in range(2,num):
+    if(num%i==0):
+        prime=False
+        break
+
+if prime:
+    print("this number is prime")
+else: 
+    print("This number is not Prime")
